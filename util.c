a64917f9cced83ebad50844fdcb4670a6847ceee
AHN Haram <160437590+HaramAHNparis8@users.noreply.github.com>
2024-12-23 19:15:26 +0100

    ll'erreur

--- util.c
+++ util.c
@@ -7,32 +7,45 @@
 
 typedef int element_entree;
 
-int entrelavaleur(){
-	element_entree n;
-	time_t debut = time(NULL);
-	time_t courant;
-	while(1){
+int entrelavaleur() {
+    element_entree n;
+    time_t debut = time(NULL);
+    time_t courant;
 
-		printf("\033[5m entrez la valeur que vous voulez ajoute.\033[0m\r\n");		
-		printf("\033[5m -> \033[0m\r\n");
-		fflush(stdout);
-		
-		usleep(clignotement);
-		printf("                        \r");
-		fflush(stdout);
-		usleep(clignotement);
-		
-		courant = time(NULL);	
-		if(courant - debut > letempsdourant){
+    while (1) {
+        courant = time(NULL);
 
-			break;
-		};
-	}
-	printf("entrez la valeur que vous voulez ajoute.\n");
-	printf("-> \n");
-	scanf("%d ", &n);
-	return n;
-	
+       
+        if (courant - debut >= letempsdourant) {
+            break;
+        }
+
+        
+        printf("\033[5mEntrez la valeur que vous voulez ajouter.\033[0m\r");
+	printf("\n");
+	printf("\033[5m->\033[0m\r");
+
+        fflush(stdout); 
+
+        usleep(clignotement); 
+
+     
+        printf("\r                        \r");
+        fflush(stdout);
+
+        usleep(clignotement); 
+    }
+
+    printf("Entrez la valeur que vous voulez ajouter.\n");
+    printf("-> ");
+    fflush(stdout); 
+
+    if (scanf("%d", &n) != 1) { 
+        printf("Erreur d'entrée. Valeur par défaut utilisée: 0\n");
+        return 0;
+    }
+
+    return n;
 }
 
 
