36b5af852bea6341b4b72bad23971f08045f57bf
AHN Haram <160437590+HaramAHNparis8@users.noreply.github.com>
2024-12-23 20:14:24 +0100

    l'erreur fixe mais c'est pas mon estimation

--- util.c
+++ util.c
@@ -1,57 +1,62 @@
 #include <stdio.h>
-#include <unistd.h> // sleep(), usleep()
-#include <time.h> // time
+#include <unistd.h>  // sleep(), usleep()
+#include <time.h>    // time()
 
-#define clignotement 500000
-#define letempsdourant 5
+#define clignotement 500000 
+#define letempsdourant 5    
 
-typedef int element_entree;
+typedef int element_int;
 
-int entrelavaleur() {
-    element_entree n;
+
+void AfficherLeMessage(void (*f)(void)) {
     time_t debut = time(NULL);
     time_t courant;
 
     while (1) {
         courant = time(NULL);
 
-       
+        
         if (courant - debut >= letempsdourant) {
             break;
         }
 
         
-        printf("\033[5mEntrez la valeur que vous voulez ajouter.\033[0m\r");
-	printf("\n");
-	printf("\033[5m->\033[0m\r");
+        (*f)();
 
-        fflush(stdout); 
+        fflush(stdout);  
 
         usleep(clignotement); 
 
-     
+        
         printf("\r                        \r");
         fflush(stdout);
 
-        usleep(clignotement); 
+        usleep(clignotement);  
     }
 
-    printf("Entrez la valeur que vous voulez ajouter.\n");
-    printf("-> ");
-    fflush(stdout); 
-
-    if (scanf("%d", &n) != 1) { 
-        printf("Erreur d'entrée. Valeur par défaut utilisée: 0\n");
-        return 0;
-    }
-
-    return n;
+    (*f)();
+    fflush(stdout);
 }
 
 
-int main(void){
-	int n;
-	n = entrelavaleur();
-	printf("\n res : %d\n", n);
-	return 0;
+void afficherMessageMachain() {
+    printf("Entrez la valeur que vous voulez ajouter.");
+}
+
+int entrelavaleur() {
+    
+    element_int n;
+    printf("\n");
+    AfficherLeMessage(afficherMessageMachain);   
+    printf("\n->  ");
+    fflush(stdout);
+    scanf("%d", &n);
+  
+    return n;
+}
+
+int main(void) {
+    int n = entrelavaleur(); 
+    printf("\nRésultat : %d\n", n);  
+    return 0;
 }
